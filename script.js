4528e0db18f67b6bce9924221c9b88544bf7b382
TalelKarim <talelchebbi090@gmail.com>
2022-03-15 23:35:08 +0100

    final commit

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-var currentTime = new Date();
-
-var hours = currentTime.getHours();
-var minutes = currentTime.getMinutes();
-var seconds = currentTime.getSeconds();
 
 const heures = document.querySelector('.hours');
 const minutess = document.querySelector('.minutes');
 const secondes = document.querySelector('.seconds');
 
-//set minutes 
+
+function showCurrentTime(){
+    var currentTime = new Date();
+
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+
+    //set minutes 
 if (minutes <10 ){
     minutes = "0" + minutes; 
 }
@@ -23,6 +26,12 @@ if(hours < 10){
     hours = "0" + hours;
 }
 
-heures.textContent = hours;
-minutess.textContent = minutes;
-secondes.textContent = seconds;
+heures.innerText = hours ;
+minutess.innerText =  minutes ;
+secondes.innerText = seconds;
+
+}
+
+
+showCurrentTime();
+setInterval(showCurrentTime, 1000);
